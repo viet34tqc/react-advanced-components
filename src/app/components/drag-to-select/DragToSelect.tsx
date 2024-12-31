e6a5b3379afca4a959403595123111156864dba3
viet34tqc <viet34tqc@gmail.com>
2024-12-31 21:06:37 +0700

    Update drag to select

--- src/app/components/drag-to-select/DragToSelect.tsx
+++ src/app/components/drag-to-select/DragToSelect.tsx
@@ -30,6 +30,7 @@ class DOMVector {
     this.magnitudeY = magnitudeY;
   }
 
+  // This is the DOMRect that represents the selection box
   toDOMRect(): DOMRect {
     return new DOMRect(
       // When we drag from right to left, we need to reduce the x value, that's when this.x + this.magnitudeX < this.x
@@ -41,13 +42,23 @@ class DOMVector {
   }
 }
 
+const getDiagonalLength = (x: number, y: number): number => {
+  // This is the Pythagorean theorem: a^2 + b^2 = c^2
+  // We want to find the length of the diagonal, which is c
+  return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+};
+
 const DragToSelect = () => {
   const [dragVector, setDragVector] = useState<DOMVector | null>(null);
+  const [isDragging, setIsDragging] = useState(false);
   const [selectedItems, setSelectedItems] = useState<Record<string, boolean>>(
     {}
   );
-  const selectionRect = dragVector ? dragVector.toDOMRect() : null;
   const containerRef = useRef<HTMLDivElement>(null);
+
+  // This is the DOMRect that represents the selection box
+  const selectionRect = dragVector ? dragVector.toDOMRect() : null;
+
   const updateSelectedItems = (dragVector: DOMVector) => {
     if (containerRef.current === null) return;
     const result: Record<string, boolean> = {};
@@ -58,14 +69,15 @@ const DragToSelect = () => {
 
       const x = itemRect.x - containerRect.x;
       const y = itemRect.y - containerRect.y;
-      const translatedItemRect = new DOMRect(
+
+      const itemRectInsideContainer = new DOMRect(
         x,
         y,
         itemRect.width,
         itemRect.height
       );
 
-      if (!isIntersecting(dragVector.toDOMRect(), translatedItemRect)) {
+      if (!isIntersecting(dragVector.toDOMRect(), itemRectInsideContainer)) {
         return;
       }
 
@@ -97,6 +109,10 @@ const DragToSelect = () => {
         0
       )
     );
+
+    // Only trigger pointer event (e.pointerId) for this element (e.currentTarget)
+    // This prevent non-pointer events (ex: hover) from triggering while we dragging
+    e.currentTarget.setPointerCapture(e.pointerId);
   };
 
   const handlePointerMove = (e: React.PointerEvent<HTMLDivElement>) => {
@@ -104,21 +120,46 @@ const DragToSelect = () => {
 
     const containerRect = e.currentTarget.getBoundingClientRect();
 
+    // We focus on the container so that we can listen to ESC key events to cancel the drag
+    containerRef.current?.focus();
+
     // e.client and containerRect are relative to the window, so we need to calculate the position of the current pointer relative to the container first
     // Think of it like this: edge of window ---- container edge ---- pointer location
     const x = e.clientX - containerRect.x;
     const y = e.clientY - containerRect.y;
 
+    // Now we calculate how long the pointer has moved, which is the magnitude of the vector
+    const nextMagnitudeX = x - dragVector.x;
+    const nextMagnitudeY = y - dragVector.y;
+
     const nextDragVector = new DOMVector(
       dragVector.x,
       dragVector.y,
-      // Now we calculate how long the pointer has moved, which is the magnitude of the vector
-      x - dragVector.x,
-      y - dragVector.y
+      nextMagnitudeX,
+      nextMagnitudeY
     );
+
+    // Prevent premature dragging, like clicking on the item
+    // we only want to start dragging when the pointer has moved at least 10px
+    if (!isDragging && getDiagonalLength(nextMagnitudeX, nextMagnitudeY) < 10) {
+      return;
+    }
+    setIsDragging(true);
 
     setDragVector(nextDragVector);
     updateSelectedItems(nextDragVector);
+  };
+
+  const handlePointerUp = () => {
+    // This happens when we drag and release the pointer immediately
+    if (!isDragging) {
+      setSelectedItems({});
+      setDragVector(null);
+    } else {
+      // When isDragging is true and we release the pointer, we want to reset the drag vector
+      setDragVector(null);
+      setIsDragging(false);
+    }
   };
 
   return (
@@ -132,18 +173,27 @@ const DragToSelect = () => {
         )}
       </div>
       <div
-        className="relative z-0 grid grid-cols-8 sm:grid-cols-10 gap-4 p-4 border-2 border-black -translate-y-0.5"
+        className="relative z-0 grid grid-cols-8 sm:grid-cols-10 gap-4 p-4 border-2 border-black select-none focus:outline-none focus:border-dashed  -translate-y-0.5"
         onPointerDown={handlePointerDown}
         onPointerMove={handlePointerMove}
-        onPointerUp={() => setDragVector(null)}
+        onPointerUp={handlePointerUp}
         ref={containerRef}
+        // We need tabIndex to -1 so that containerRef.current, which is a non-focusable element, can be focused programmatically
+        tabIndex={-1}
+        onKeyDown={e => {
+          if (e.key === 'Escape') {
+            e.preventDefault();
+            setSelectedItems({});
+            setDragVector(null);
+          }
+        }}
       >
         {items.map(item => (
           <div
-            className={`border-2 size-10 border-black flex justify-center items-center ${
+            className={`border-2 size-10 border-slate-900 flex justify-center items-center hover:bg-pink-500 ${
               selectedItems[item]
-                ? 'bg-black text-white'
-                : 'bg-white text-black'
+                ? 'bg-slate-900 text-white'
+                : 'bg-white text-slate-900'
             }`}
             data-item={item}
             key={item}
--- src/app/components/drag-to-select/README.md
+++ src/app/components/drag-to-select/README.md
@@ -1,5 +1,3 @@
 # Drag to select
 
-In this component, you can select a range of elements by dragging the mouse.
-
-- We need to draw a rectangle on the screen as the user drags the mouse.
+In this component, you can select a range of elements by dragging the mouse.We need to draw a rectangle on the screen as the user drags the mouse. This is done by using `DOMRect` api
